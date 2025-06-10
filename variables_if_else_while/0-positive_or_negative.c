8b1ff9c298f6f4e8d117a14f0b1bcce5b8c40549
lxcario <resquedzn05@gmail.com>
2025-06-10 09:57:24 +0000

    0-positive_or_negative.c: Script for C

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h> /* Required for printf function */
+
+/**
+ * main - Entry point
+ *
+ * Description: This program assigns a random number to n and prints whether
+ * it is positive, negative, or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int n;
+
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+
+    /* Your code goes here */
+    printf("%d ", n); /* Print the number followed by a space */
+
+    if (n > 0)
+    {
+        printf("is positive\n");
+    }
+    else if (n == 0)
+    {
+        printf("is zero\n");
+    }
+    else /* n < 0 */
+    {
+        printf("is negative\n");
+    }
+
+    return (0);
+}
