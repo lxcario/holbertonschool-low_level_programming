b52ed7d9f4dd706f7ff743c7694a9b38d64825f1
lxcario <resquedzn05@gmail.com>
2025-06-10 11:16:58 +0000

    Yes

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -9,15 +9,17 @@
  {
     char c;
 
-    for (c= 'a'; c<= 'z'; c++);
+    for (c = 'a'; c <= 'z'; c++);
     {
         putchar(c);
     }
-    for (c= 'A'; c<= 'Z'; c++);
+
+    for (c = 'A'; c <= 'Z'; c++);
     {
         putchar(c);
     }
 
     putchar('\n');
+
     return (0);
 }
