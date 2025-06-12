fe713a4a5d8c52f3fdccb17a513cbadcb601d686
lxcario <resquedzn05@gmail.com>
2025-06-12 13:07:33 +0300

    yes

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower(int c) - checks for lowercase
+ *
+ * DESC -  Return 1 if c is lowercase
+ *
+ * Return: Always Success
+ */
+int _islower(int c)
+{
+	if (c >= 'a'; && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return(0);
+	}
+}
+
