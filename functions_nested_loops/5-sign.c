5fd42503fa53be4207a6a70e2b3a0f3510092e62
root <root@Resque.localdomain>
2025-06-13 02:21:24 +0300

    yes

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,28 +1,28 @@
 #include "main.h"
 
 /**
- * print_sign - prints the sign of a number
+ * print_sign - Prints the sign of a number.
+ * @n: The number of which the sign will be printed.
  *
- * n - a number
- *
- * Return: 1 if n is greater than zero:
+ * Return: 1 if the number is greater than zero,
+ *         0 if the number is zero,
+ *         -1 if the number is less than zero.
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar ('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar ('0');
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
-		_putchar ('-');
+		_putchar('-');
 		return (-1);
 	}
-
 }
