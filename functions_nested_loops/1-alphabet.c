e34555bbd421becc5e3519d6cf5a3e25d6fb919f
lxcario <resquedzn05@gmail.com>
2025-06-12 12:19:43 +0300

    yes

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+ *
+ * print_alphabet - Print alphabet on lowercase
+ *
+ * Return - void (no return value)
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+
+	_putchar ('\n');
+}
+	
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -4,4 +4,6 @@
 /* Function prototype for _putchar */
 int _putchar(char c);
 
+void print_alphabet(void);
+
 #endif /* MAIN_H */
