e2f0deff5228e27a640c442865eb80bffc163e31
lxcario <resquedzn05@gmail.com>
2025-06-12 12:55:32 +0300

    yes

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,24 @@
-#include "main.h"
+#include "main.h" /* Always use double quotes for custom header files */
 
 /**
- * print_alphabet_x10 - print alphabet 10 times
+ * print_alphabet_x10 - Prints the lowercase alphabet 10 times.
  *
- * Return void
+ * Description: This function uses a nested loop structure to print the
+ * lowercase alphabet from 'a' to 'z' ten times, each followed by a new line.
+ * It strictly uses the _putchar function for character output.
+ *
+ * Return: void.
  */
 void print_alphabet_x10(void)
 {
-	int count
-	char letter
+	int count;
+	char letter;
 
-	for (count = 0; count < 10; count++);
+	for (count = 0; count < 10; count++)
 	{
-		for (letter = 'a'; letter <= ''z'; letter++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 		{
-			_putchar(letter)
+			_putchar(letter);
 		}
 		_putchar('\n');
 	}
