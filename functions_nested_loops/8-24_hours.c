c366c209b70496ac6e3bf3615fa6e33869e39135
lxcario <resquedzn05@gmail.com>
2025-06-15 06:35:27 +0000

    yes

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,31 +1,22 @@
-#include "main.h" // Assuming main.h declares _putchar
+#include "main.h"
 
 /**
- * jack_bauer - Prints every minute of the day of Jack Bauer,
- * starting from 00:00 to 23:59.
- *
- * Return: void
+ * jack_bauer - Prints every minute of the day of
+ *              Jack Bauer, starting from 00:00 to 23:59.
  */
 void jack_bauer(void)
 {
-	int hour;
-	int minute;
+	int hour, minute;
 
-	for (hour = 0; hour < 24; hour++)
+	for (hour = 0; hour <= 23; hour++)
 	{
-		for (minute = 0; minute < 60; minute++)
+		for (minute = 0; minute <= 59; minute++)
 		{
-			// Print the tens digit of the hour
 			_putchar((hour / 10) + '0');
-			// Print the units digit of the hour
 			_putchar((hour % 10) + '0');
-			// Print the colon separator
 			_putchar(':');
-			// Print the tens digit of the minute
 			_putchar((minute / 10) + '0');
-			// Print the units digit of the minute
 			_putchar((minute % 10) + '0');
-			// Print a newline character after each minute
 			_putchar('\n');
 		}
 	}
