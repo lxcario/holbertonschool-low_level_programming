9db289d7fe8abc7fad1670d382daca5c6aedd71a
root <root@Resque.localdomain>
2025-06-23 17:53:22 +0300

    yes

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,22 +1,29 @@
-#include "main.h" /* "main.h" dosyasının mevcut olduğunu varsayıyoruz */
+#include "main.h" /* Assuming "main.h" file exists */
 
 /**
- * _memset - Bellek alanını sabit bir bayt ile doldurur.
- * @param s: Doldurulacak bellek alanının başlangıç adresi (işaretçi).
- * @param b: Bellek alanına yazılacak sabit bayt değeri.
- * @param n: Doldurulacak bayt sayısı.
+ * _memset - Fills memory with a constant byte.
+ * @s: Pointer to the memory area to be filled.
+ * @b: The constant byte to fill the memory area with.
+ * @n: The number of bytes to be filled.
  *
- * @return: Bellek alanı s'ye bir işaretçi döndürür.
+ * Return: A pointer to the memory area s.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i; /* Döngü için sayaç değişkeni */
+	unsigned int i; /* Loop counter variable */
 
+	/* Loop to fill the memory area for n bytes */
 	for (i = 0; i < n; i++)
 	{
+		/*
+		 * Fill the i-th byte starting from address s with value b.
+		 * s[i] corresponds to address s + i * sizeof(char),
+		 * and since char is 1 byte, it can be thought of as just s + i.
+		 */
 		s[i] = b;
 	}
 
+	/* Return the pointer to the filled memory area */
 	return (s);
 }
 
