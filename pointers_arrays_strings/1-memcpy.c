57080993df4823d8faeba955d553f52043caae57
root <root@Resque.localdomain>
2025-06-23 18:56:23 +0300

    yes

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _memcpy - function copies n bytes from memory area src to memory area dest
- * @n:bytes from memory area
- * @src: memory area
- * @dest: yes
+ * _memcpy - Copies n bytes from memory area src to memory area dest.
+ * @dest: The destination memory area.
+ * @src: The source memory area.
+ * @n: The number of bytes to copy.
  *
- * Return: A pointer to the destination memory area dest. 
+ * Return: A pointer to the destination memory area dest.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
