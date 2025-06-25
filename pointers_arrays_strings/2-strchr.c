c5e0e5abff82c9caf597acc54c73e4a20ca7c5c2
lxcario <resquedzn05@gmail.com>
2025-06-25 13:20:46 +0000

    yes

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - function returns a pointer to the first occurrence of the character c in the string s
+ *
+ * @c: character
+ * @s: string
+ * @NULL: character not found
+ *
+ * Return : pointer to the first occurrence of the character
+ */
+char *_strchr(char *s, char c);
+{
+	do
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	}
+	while (*s++);
+
+	return (NULL);
+}
