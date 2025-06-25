0d540beabdd1b4a4e0a054dafb83fee158171bb1
lxcario <resquedzn05@gmail.com>
2025-06-25 13:34:13 +0000

    yes

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -6,7 +6,7 @@
  * @c: character
  * @s: string
  *
- * Return : pointer to the first occurrence of the character, NULL if the character is not found.
+ * Return : pointer to, NULL if the character is not found.
  */
 char *_strchr(char *s, char c)
 {
