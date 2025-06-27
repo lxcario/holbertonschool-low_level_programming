e7ba42d8ca3bca875f10884fdb70310e75b09b93
lxcario <resquedzn05@gmail.com>
2025-06-27 16:01:21 +0000

    yes

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include <stddef.h> // Required for NULL
+
+/**
+ * _strpbrk - Locates the first occurrence in the string s of any of the bytes
+ * in the string accept.
+ * @s: The string to be searched.
+ * @accept: The string containing the bytes to match.
+ *
+ * Return: A pointer to the byte in s that matches one of the bytes in accept,
+ * or NULL if no such byte is found.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+		}
+	}
+
+	return (NULL);
+}
+
