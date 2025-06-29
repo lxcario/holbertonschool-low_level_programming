9bc611b714cd4905b07ae485ea723ddcffb85396
lxcario <resquedzn05@gmail.com>
2025-06-29 17:28:03 +0000

    yes

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -100,4 +100,18 @@ void print_chessboard(char (*a)[8]);
 
 void print_diagsums(int *a, int size);
 
+void _puts_recursion(char *s);
+
+void _print_rev_recursion(char *s);
+
+int _strlen_recursion(char *s);
+
+int factorial(int n);
+
+int _pow_recursion(int x, int y);
+
+int _sqrt_recursion(int n);
+
+int is_prime_number(int n);
+
 #endif /* MAIN_H */
