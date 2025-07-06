bc89e395af77d178ba6b49b8069908bf3a0e5dfe
lxcario <resquedzn05@gmail.com>
2025-07-06 19:21:22 +0000

    yes

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a dog's basic information
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: This structure defines the basic attributes of a dog.
+ */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+    char *name;  // These lines should typically have 4 spaces of indentation
+    float age;   // if they are inside a block.
+    char *owner; // The warning "no spaces at the start of a line" might be misleading
+                 // or referring to something else if these lines are meant to be indented.
 };
 
 #endif /* DOG_H */
