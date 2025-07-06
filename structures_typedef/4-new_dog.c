709427a62266e43bd3cfc7207c1f7ffbd5dbbb6e
lxcario <resquedzn05@gmail.com>
2025-07-06 19:41:22 +0000

    yes

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,42 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
+
+/**
+ * _strlen - calculates the length of a string.
+ * @s: The string to measure.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strcpy - copies a string from source to destination.
+ * @dest: The buffer to copy the string to.
+ * @src: The string to be copied.
+ *
+ * Return: A pointer to the destination string @dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
 
 /**
  * new_dog - creates a new dog
@@ -15,8 +51,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *d;
 	int name_len, owner_len;
 
-	name_len = strlen(name);
-	owner_len = strlen(owner);
+	name_len = _strlen(name);
+	owner_len = _strlen(owner);
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
@@ -28,7 +64,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(d);
 		return (NULL);
 	}
-	strcpy(d->name, name);
+	_strcpy(d->name, name);
 
 	d->owner = malloc(sizeof(char) * (owner_len + 1));
 	if (d->owner == NULL)
@@ -37,9 +73,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(d);
 		return (NULL);
 	}
-	strcpy(d->owner, owner);
+	_strcpy(d->owner, owner);
 
 	d->age = age;
 
 	return (d);
 }
+
