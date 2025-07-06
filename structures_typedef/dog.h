70e664bb9ab87742410c8b4b1b6ccd00ac701440
lxcario <resquedzn05@gmail.com>
2025-07-06 19:39:55 +0000

    yes

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -20,8 +20,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
 
-dog_t *new_dog(char *name, float age, char *owner);
-
 /**
  * dog_t - typedef for struct dog
  */
@@ -29,5 +27,7 @@ typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
