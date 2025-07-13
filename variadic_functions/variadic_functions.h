3e66ce0f133353ae973f4a5c77ff974a8c78b7c5
root <resquedzn05@gmail.com>
2025-07-13 18:53:01 +0000

    yes

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+
+/*
+ * File: variadic_functions.h
+ * Desc: Header file containing prototypes for all functions
+ * in the 0x0F-variadic_functions directory.
+ */
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
