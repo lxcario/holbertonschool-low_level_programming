9130e1cfa5a85d092380bd82b8bd87cf1bf071aa
root <resquedzn05@gmail.com>
2025-07-13 17:57:40 +0000

    yes

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -2,6 +2,8 @@
 #define _FUNCTION_POINTERS_H_
 
 
+void array_iterator(int *array, size_t size, void (*action)(int));
+
 void print_name(char *name, void (*f)(char *));
 
 #endif /* _FUNCTION_POINTERS_H_ */
