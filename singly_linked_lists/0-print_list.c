5cdd0b4300bc977db38415927ac96d054b997cbc
lxcario <resquedzn05@gmail.com>
2025-07-24 12:36:29 +0000

    yes

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,18 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/**
- * struct list_s - singly linked list
- * @str: string
- * @len: length
- * @next: next node
- */
-typedef struct list_s
-{
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
+#include "lists.h"
 
 /**
  * print_list - Prints all elements of a list_t list.
@@ -21,22 +9,21 @@ typedef struct list_s
  */
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
-    const list_t *current = h;
+	size_t count = 0;
+	const list_t *current = h;
 
-    while (current != NULL)
-    {
-        if (current->str == NULL)
-        {
-            printf("[0] (nil)\n");
-        }
-        else
-        {
-            printf("[%u] %s\n", current->len, current->str);
-        }
-        current = current->next;
-        count++;
-    }
-    return (count);
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", current->len, current->str);
+		}
+		current = current->next;
+		count++;
+	}
+	return (count);
 }
-
