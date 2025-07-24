715028203b72c73d31f8511e9877cf31902f0827
lxcario <resquedzn05@gmail.com>
2025-07-24 12:37:23 +0000

    yes

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -5,20 +5,17 @@
 
 /**
  * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
+ * @str: string
+ * @len: length
+ * @next: next node
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
 
 #endif
-
